f3ba4a529def61b887de33cac59e098742724283
Sebzlol <pwnage@live.dk>
2018-09-21 09:02:22 +0200

    MapView and marker fixed

--- app/src/main/java/systems/mobile/vildmad/MainActivity.java
+++ app/src/main/java/systems/mobile/vildmad/MainActivity.java
@@ -1,8 +1,8 @@
 package systems.mobile.vildmad;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -10,13 +10,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.*;
-import com.google.android.gms.maps.MapFragment;
-
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-    private Fragment fragment = null;
+    Fragment fragment = null;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -28,9 +26,8 @@ public class MainActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_map:
-                    //mTextMessage.setText(R.string.title_map);
                     fragment = new MapFragment();
-                    startFragment(fragment);
+                    replaceFragment(fragment);
                     return true;
                 case R.id.navigation_landscape:
                     mTextMessage.setText(R.string.title_landscape);
@@ -57,16 +54,10 @@ public class MainActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    public void startFragment(Fragment startFragment){
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.add(R.id.container, startFragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
-    }
-
-    public void replaceFragment(Fragment newFragment){
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, newFragment);
+    public void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.container, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
