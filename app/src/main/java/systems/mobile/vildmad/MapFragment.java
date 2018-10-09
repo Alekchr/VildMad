f2415f009b03b4b18f4f4ccf876a6c7c5b1a8e6d
jpniels <jonasphillipn@gmail.com>
2018-10-09 11:03:38 +0200

    button to get to last location works but bad code!

--- app/src/main/java/systems/mobile/vildmad/MapFragment.java
+++ app/src/main/java/systems/mobile/vildmad/MapFragment.java
@@ -1,32 +1,53 @@
 package systems.mobile.vildmad;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback{
+import java.util.ArrayList;
 
-    GoogleMap mGooglemap;
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mGooglemap;
     MapView mMapView;
     View mView;
+    private Button button;
+    ArrayList<MarkerOptions> markerArray = new ArrayList<MarkerOptions>();
+
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private FusedLocationProviderClient mFusedLocationClient;
 
     public MapFragment() {
         // Required empty public constructor
@@ -47,12 +68,15 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 
     @Override
@@ -60,28 +84,118 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_map, container, false);
+        button = (Button) mView.findViewById(R.id.setMarker);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+
+        }
+
+
+
+
+
         return mView;
     }
 
     @Override
-    public void onViewCreated( View view, Bundle savedInstanceState) {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        markerArray = new ArrayList<>();
         mMapView = (MapView) mView.findViewById(R.id.map);
-        if(mMapView != null) {
+        if (mMapView != null) {
             mMapView.onCreate(null);
             mMapView.onResume();
             mMapView.getMapAsync(this);
         }
     }
+
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         MapsInitializer.initialize(getContext());
         mGooglemap = googleMap;
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.689247, -74.044502)).title("This is testing").snippet("Test2"));
-        CameraPosition testpos = CameraPosition.builder().target(new LatLng(40.689247, -74.044502)).zoom(16).bearing(0).tilt(45).build();
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(testpos));
+        
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                }
+                mFusedLocationClient.getLastLocation()
+                        .addOnCompleteListener(getActivity(), new OnCompleteListener<Location>() {
+
+                            @Override
+                            public void onComplete(@NonNull Task<Location> task) {
+                                MarkerOptions marker = createMarker(task.getResult().getLatitude(), task.getResult().getLongitude(), "hej", "hej");
+                                markerArray.add(marker);
+                                googleMap.addMarker(markerArray.get(0));
+                                CameraPosition testpos = CameraPosition.builder().target(new LatLng(markerArray.get(0).getPosition().latitude, markerArray.get(0).getPosition().longitude)).zoom(16).bearing(0).tilt(45).build();
+
+                                googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(testpos));
+                            }
+
+
+                        });
+                // Code here executes on main thread after user presses button
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
+    public MarkerOptions createMarker(double latitude, double longitude, String title, String snippet) {
+        return new MarkerOptions()
+                .position(new LatLng(latitude, longitude))
+                .anchor(0.5f, 0.5f)
+                .title(title)
+                .snippet(snippet);
+    }
+
+    public void getPosition() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+        }
+
+
+        mFusedLocationClient.getLastLocation()
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<Location>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        MarkerOptions marker = createMarker(task.getResult().getLatitude(), task.getResult().getLongitude(), "hej", "hej");
+                        markerArray.add(marker);
+
+                    }
+
+
+                });
+
+
+}
+
+
+
 
 
 
