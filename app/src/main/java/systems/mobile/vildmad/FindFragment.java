54923a2c6b120f8773569208a9e6e2180744d666
ChangHuii <alek.chr@hotmail.com>
2018-12-05 15:18:28 +0100

    Added FindFragment for list of plants

---
+++ app/src/main/java/systems/mobile/vildmad/FindFragment.java
@@ -0,0 +1,64 @@
+package systems.mobile.vildmad;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+public class FindFragment extends Fragment{
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private ListView mListView;
+
+    public FindFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FindFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FindFragment newInstance(String param1, String param2) {
+        FindFragment fragment = new FindFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view;
+        view = inflater.inflate(R.layout.fragment_add_picture, container, false);
+
+        return view;
+    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+
+    }
+
+}
