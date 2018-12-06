58fe2e840955a21b9abfc81379a4ee584524eef9
ChangHuii <alek.chr@hotmail.com>
2018-12-06 09:45:45 +0100

    Addded getters and setters for title and description

--- app/src/main/java/systems/mobile/vildmad/CustomMarker.java
+++ app/src/main/java/systems/mobile/vildmad/CustomMarker.java
@@ -52,4 +52,19 @@ public class CustomMarker {
         this.pictureUrl = pictureUrl;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
